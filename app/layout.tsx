70174cdd36c57a23133dc127c14d68f39309a863
Rajmehra725 <nagiyaraj.725@gmail.com>
2025-05-03 11:10:24 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import Navbar from '@/app/cs/components/Navbar';
 import Footer from '@/app/cs/components/Footer';
-
+import { Analytics } from "@vercel/analytics/react"
 export const metadata = {
   title: 'RaazGateX',
   description: 'GATE + Adventure + Learning Platform by Raaz Mehra',
